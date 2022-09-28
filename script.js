adf389d8099d6e12bd01bd362512596e885a3da8
jessy hazart <jessy.hazart@gmail.com>
2022-09-28 12:26:56 +0200

    add: js progress bar/ css for timeline/ div class infos

---
+++ script.js
@@ -0,0 +1,17 @@
+//Define start & end date
+let start = new Date("Sep 12, 2022").getTime();
+let end = new Date("Feb 10, 2023").getTime();
+
+//Calculate time between start and end 
+let totalDays = end - start;
+
+//Calculate time spend since the start
+let daysDone = new Date().getTime() - start;
+
+//Calculate the pourcentage of time spend
+let pourcentage = daysDone * 100 / totalDays;
+
+//Select the class of the progress bar
+let progressBar = document.querySelector('.progress');
+
+daysDone < totalDays ? progressBar.style.height = `${parseInt(pourcentage)}%` : progressBar.style.height = "100%";
