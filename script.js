a3b0c7c2caad09b8777fb73efc3da616a7977bae
Oliv751 <olivierlopez751@gmail.com>
2022-09-30 10:35:18 +0200

    modif header

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-// Header
-const button = document.getElementById("setting-button");
-button.addEventListener("click", () => {
-    const value = button.innerText;
-
-    if (value === "settings") {
-        button.innerText = "close"
-    }
-    else {
-        button.innerText = "settings"
-    }
-
-})
 // timeline
 
 //Define start & end date
